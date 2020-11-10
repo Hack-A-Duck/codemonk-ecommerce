816b71e222ec0da34b3b854f2cc9f4137cbba2b1
Guneet Gulati <guneetgulati01@gmail.com>
2020-11-10 20:42:02 +0530

    backend_py

---
+++ templates/seller.php
@@ -0,0 +1,18 @@
+
+<html>
+<head>
+    <title>Testing</title>
+</head>
+<body>
+<form action="/seller" method="post">
+    Name : <input type="text" name="prodName"> <br>
+    Price : <input type="number" name="price"> <br>
+    <button type="submit" name="submit">Submit</button>
+</form>
+
+
+
+</body>
+</html>
+
+
