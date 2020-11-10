228698bdd6022f28cbe7d75ac4abe54990461c8d
Guneet Gulati <guneetgulati01@gmail.com>
2020-11-10 23:03:31 +0530

    cpjs revert

--- src/CartProduct.js
+++ src/CartProduct.js
@@ -13,24 +13,36 @@ function CartProduct({id,image,title,price}) {
                type:'REMOVE_PRODUCT',
                id:id
            })
+        }
+        return (
+            <div className="product1">
+            
+                <div className="cartproduct">
+                <img className="cartproduct_image" src={image} />
+                <div className="cartproduct_info">
+                <p className="carttext">{title}</p>
+                <p>{title}</p>
+                <div>₹ {price}</div>
+                
+                <div className="price">₹ {price}</div>
+                <br></br><br></br><br></br><br></br>
+                <Button className="remove"  onClick={removeProduct} variant="contained" color="secondary">
+                     Remove 
+                </Button>
+                   
+                </div>
+                
+                </div>
+                
+                <Button onClick={removeProduct} variant="contained" color="secondary">
+                     Remove 
+                </Button>
+                
+    
+            </div>
+            
+        )
     }
-    return (
-        <div>
-            <div className="cartproduct">
-            <img className="cartproduct_image" src={image} />
-            <div className="cartproduct_info">
-            <p>{title}</p>
-            <div>₹ {price}</div>
-            
-            </div>
-            </div>
-            <Button onClick={removeProduct} variant="contained" color="secondary">
-                 Remove 
-            </Button>
-            
-
-        </div>
-    )
-}
+    
 
 export default CartProduct;
